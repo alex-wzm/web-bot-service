c2e3c1f6bb19b01ffc5ca2ce7e334b73ccaf935c
Alex Wong <alex-wzm@outlook.com>
2020-10-08 23:02:43 -0400

    simplify getProjectID func definition

--- main.go
+++ main.go
@@ -20,29 +20,14 @@ func getProjectID() string {
 		fmt.Print(err)
 	}
 
-	type ServiceAccountJSONFile struct {
-		FileType                string `json:"type"`
-		ProjectID               string `json:"project_id"`
-		PrivateKeyID            string `json:"private_key_id"`
-		PrivateKey              string `json:"private_key"`
-		ClientEmail             string `json:"client_email"`
-		ClientID                string `json:"client_id"`
-		AuthURI                 string `json:"auth_uri"`
-		TokenURI                string `json:"token_uri"`
-		AuthProviderX509CertURL string `json:"auth_provider_x509_cert_url"`
-		ClientX509CertURL       string `json:"client_x509_cert_url"`
-	}
+	var parsed map[string]interface{}
 
-	var obj ServiceAccountJSONFile
-
-	err = json.Unmarshal(data, &obj)
+	err = json.Unmarshal(data, &parsed)
 	if err != nil {
-		fmt.Println("error:", err)
+		fmt.Print(err)
 	}
 
-	fmt.Printf("Project ID: %s\n", obj.ProjectID)
-	return obj.ProjectID
-
+	return parsed["project_id"].(string)
 }
 
 // detectIntent gets a response to a text query using the Dialogflow detectIntent API
